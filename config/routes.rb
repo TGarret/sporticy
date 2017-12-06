93c0e86be774f28317524f4d9fbe942fe8a77e7a
Yohan Neris <Yohanneris@macbook-air-de-yohan.home>
2017-12-06 12:55:25 +0100

    device +route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  devise_for :users,
+               :path => '',
+               :path_names => {:sign_in => 'login', :sign_out => 'logout', :edit => 'profile'}
+
 root 'pages#home'
 
 end
