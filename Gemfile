36c09880a4f0746a26320dabc1171687c5aee89e
Yohan Neris <Yohanneris@macbook-air-de-yohan.home>
2017-12-06 17:11:23 +0100

    Upload photo de profi

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'turbolinks',       '5.0.1'
 gem 'jbuilder',         '2.7.0'
 gem 'devise'
 gem 'devise-i18n'
+gem 'paperclip'
 
 group :development, :test do
   gem 'sqlite3', '1.3.13'
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 validates :fullname, presence: true, length: {maximum: 65}
 
+has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" },
+:default_url => "/assets/default_image.png"
+validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
 end
