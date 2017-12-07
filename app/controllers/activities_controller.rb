db2ae82fc87920e70d1b89417071a4dbfca895c0
Tim <tim.garret@inseec-france.com>
2017-12-07 08:55:05 +0100

    creation des vues photos

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,6 +2,7 @@ class ActivitiesController < ApplicationController
 
 before_action :set_activity, only: [:show, :edit, :update]
 before_action :authenticate_user!, except: [:show]
+before_action :require_same_user, only: [:edit, :update]
 
 	def index
 		@activities = current_user.activities
@@ -14,21 +15,35 @@ before_action :authenticate_user!, except: [:show]
 	def create
 		@activity = current_user.activities.build(activity_params)
 		if @activity.save
-			redirect_to @activity, notice: "Votre activité a été ajouté avec succès"
+			if params[:images]
+				params[:images].each do |i|
+					@activity.photos.create(image: i)
+				end
+			end
+			@photos = @activity.photos
+			redirect_to edit_activity_path(@activity), notice: "Votre activité a été ajouté avec succès"
 		else
 			render :new
 		end
 	end
 
 	def show
+		@photos = @activity.photos
 	end
 
 	def edit
+		@photos = @activity.photos
 	end
 
 	def update
 		if @activity.update(activity_params)
-			redirect_to @activity, notice: "Modification enregistrée..."
+			if params[:images]
+				params[:images].each do |i|
+					@activity.photos.create(image: i)
+				end
+			end
+			@photos = @activity.photos
+			redirect_to adit_activity_path(@activity), notice: "Modification enregistrée..."
 		else
 			render :edit
 		end
@@ -41,6 +56,13 @@ private
 
 	def activity_params
 		params.require(:activity).permit(:activity_name, :description, :activity_type, :competence_developed, :max_participant, :duration, :address, :siren, :price, :active)
+	end
+
+	def require_same_user
+		if current_user.id != @activity.user_id
+			flash[:danger] = "Vous n'avez pas le droit de modifier cette page"
+			redirect_to root_path
+		end
 	end
 
 
