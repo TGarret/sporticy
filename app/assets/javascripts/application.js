cd7580498e27977d14e8c0f9682bbb9507b03c1d
Tim <tim.garret@inseec-france.com>
2017-12-07 20:22:57 +0100

    Ajout de Jquery Datepicker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require turbolinks
-//= require_tree .
 //= require jquery
-//= require toastr
+//= require jquery-ui
 //= require bootstrap-sprockets
+//= require rails-ujs
+//= require toastr
+//= require_tree .
 
 toastr.options = {
  
