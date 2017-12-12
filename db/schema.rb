2a90686a531128daddfbed86cc69e1a1d2a2eeff
Tim <tim.garret@inseec-france.com>
2017-12-12 08:58:44 +0100

    modifications devise confirmable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171211172530) do
+ActiveRecord::Schema.define(version: 20171212074901) do
 
   create_table "activities", force: :cascade do |t|
     t.string "activity_name"
@@ -103,6 +103,10 @@ ActiveRecord::Schema.define(version: 20171211172530) do
     t.datetime "avatar_updated_at"
     t.string "phone_number"
     t.text "description"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
