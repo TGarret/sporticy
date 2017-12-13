5b1c69c07462572a001c67e38a99f1f60ed81770
Tim <tim.garret@inseec-france.com>
2017-12-13 15:58:03 +0100

    sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,8 @@ ActionMailer::Base.smtp_settings = {
 	:address => 'smtp.sendgrid.net',
 	:port => '587',
 	:authentication => :plain,
-	:user_name => ENV["SENDGRID_USERNAME"],
-	:password => ENV['SENDGRID_PASSWORD'],
+	:user_name => ENV["app82957879@heroku.com"],
+	:password => ENV['b7sjbd0e3323'],
 	:domain => 'heroku.com',
 	:enable_starttls_auto => true
 }
