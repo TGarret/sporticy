d0fe89876c92dcf31b230176451f40511ae72b52
Tim <tim.garret@inseec-france.com>
2017-12-14 14:39:24 +0100

    mail :

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -21,7 +21,7 @@ end
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sende = 'Sporticy <no-reply@sporticy.com>'
+  mail : 'Sporticy <no-reply@sporticy.com>'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
