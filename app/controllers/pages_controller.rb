4d96ef9442da6f2abea6d9ab671be752162f14bb
Toumanin <gheram_@hotmail.fr>
2017-12-15 13:21:38 +0100

    push module de recherche fonctionnel

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,8 +3,6 @@ class PagesController < ApplicationController
     @activities = Activity.order("RANDOM()").limit(3)
   end
 
-
-
   def search
     if params[:search].present? && params[:search].strip != ""
       session[:sporticy_search] = params[:search]
@@ -22,18 +20,5 @@ class PagesController < ApplicationController
     @activities =  @search.result
 
     @arrActivities = @activities.to_a
-
-    if (params[:date] && !params[:date].empty?)
-      date = Date.parse(params[:date])
-
-      @activities.each do |activity|
-        not_available = activity.reservation.where("(? <= date <= ?)",
-        date).limit(1)
-
-        if not not_available.length > 0
-          @arrActivities.delete(activity)
-        end
-      end
-    end
   end
 end
