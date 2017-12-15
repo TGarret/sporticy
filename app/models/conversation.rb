248778bb85bd0dccff7e7fb3c0321f415646f831
Tim <tim.garret@inseec-france.com>
2017-12-15 08:46:16 +0100

    notification mail

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,4 +1,4 @@
-class Conversation < ApplicationRecord
+class Conversation < ActiveRecord::Base
 
   belongs_to :sender, foreign_key: :sender_id, class_name: 'User'
   belongs_to :recipient, foreign_key: :recipient_id, class_name: 'User'
